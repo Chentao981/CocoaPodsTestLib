c94a5ad7968c0b9d0774f15652fd5c7651e87674
Chentao <chentao@kaochong.com>
2019-02-15 15:12:55 +0800

    commit

---
+++ CocoaPodsTestLib/CocoaPodsTestLib.h
@@ -0,0 +1,15 @@
+//
+//  CocoaPodsTestLib.h
+//  CocoaPodsTestLib
+//
+//  Created by Chentao on 2019/2/15.
+//  Copyright © 2019 Chentao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodsTestLib : NSObject
+
+-(NSString *)version;
+
+@end
---
+++ CocoaPodsTestLib/CocoaPodsTestLib.m
@@ -0,0 +1,17 @@
+//
+//  CocoaPodsTestLib.m
+//  CocoaPodsTestLib
+//
+//  Created by Chentao on 2019/2/15.
+//  Copyright © 2019 Chentao. All rights reserved.
+//
+
+#import "CocoaPodsTestLib.h"
+
+@implementation CocoaPodsTestLib
+
+-(NSString *)version{
+    return @"1.0.0";
+}
+
+@end
